b45d52a8b40edffb021d06f30c1a7a849a4de0c6
AlekseyTn <tn.aleksey@gmail.com>
2021-01-20 11:18:35 +0300

    1. Application package structure

--- app/src/main/java/com/example/kotlingb/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/kotlingb/ui/main/MainActivity.kt
@@ -1,9 +1,10 @@
-package com.example.kotlingb
+package com.example.kotlingb.ui.main
 
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.example.kotlingb.R
 
 class MainActivity : AppCompatActivity() {
 
